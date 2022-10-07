96820e7c963b2b84df1b5fe539f30ebbca371f2e
ThOpaque_MBA <thierry.kss@posteo.net>
2022-10-08 01:01:55 +0200

    Add modules.

---
+++ YoloV1_Compagny_MealTrays/train.py
@@ -0,0 +1,163 @@
+import datetime
+from timeit import default_timer as timer
+import pickle
+import logging
+
+import torch
+
+# from utils import create_logging, device, pretty_print, update_lr, save_model, tqdm_fct
+import utils
+from yolo_loss import YoloLoss
+from mealtrays_dataset import get_training_dataset, get_validation_dataset
+from darknet import YoloV1
+from metrics import MSE, MSE_confidenceScore, class_acc
+from validation import validation_loop
+
+learning_rate = 0.001
+BATCH_SIZE = 64
+SAVE_MODEL = True
+utils.create_logging()
+device = utils.device()
+logging.info(f"Learning rate = {learning_rate}")
+logging.info(f"Batch size = {BATCH_SIZE}")
+
+model = YoloV1(in_channels=3, S=7, C=8, B=2)
+model = model.to(device)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate, weight_decay=0.0005)
+loss_yolo = YoloLoss(lambd_coord=5, lambd_noobj=0.5, S=6, device=device)
+logging.info(f"Using optimizer : {optimizer}")
+
+training_dataset = get_training_dataset()
+validation_dataset = get_validation_dataset()
+
+################################################################################
+
+delta_time = datetime.timedelta(hours=1)
+timezone = datetime.timezone(offset=delta_time)
+
+t = datetime.datetime.now(tz=timezone)
+str_t = '{:%Y-%m-%d %H:%M:%S}'.format(t)
+print(f"[START] : {str_t} :")
+print(f"[Training on] : {str(device).upper()}")
+print(f"Learning rate : {optimizer.defaults['lr']}")
+logging.info("Start training")
+
+EPOCHS = 10
+S = 7
+
+################################################################################
+batch_total_train_loss_list = []
+batch_train_losses_list = []
+batch_train_class_acc = []
+
+batch_val_MSE_box_list = []
+batch_val_confscore_list = []
+batch_val_class_acc = []
+
+for epoch in range(EPOCHS):
+    utils.update_lr(epoch, optimizer)
+
+    begin_time = timer()
+    epochs_loss = 0.
+    
+    print("-"*20)
+    str_t = '{:%Y-%m-%d %H:%M:%S}'.format(t)
+    print(" "*5 + f"{str_t} : EPOCH {epoch+1}/{EPOCHS}")
+    print(" "*5 + f"Learning rate : lr = {optimizer.defaults['lr']}")
+    print("-"*20)
+
+    #########################################################################
+
+    for batch, (img, target) in utils.tqdm_fct(training_dataset):
+        model.train()
+        loss = 0
+        begin_batch_time = timer()
+        img, target = img.to(device), target.to(device)
+        
+        ### clear gradients
+        optimizer.zero_grad()
+        
+        ### prediction (N,S,S,5) & (N,S,S,10)
+        output = model(img)
+        
+        ### compute losses over each grid cell for each image in the batch
+        losses, loss = loss_yolo(output, target)
+    
+        ### compute gradients
+        loss.backward()
+        
+        ### Weight updates
+        optimizer.step()
+
+        ##### Class accuracy
+        train_classes_acc = class_acc(bbox_true, labels, label_preds)
+
+        ######### print part #######################
+        current_loss = loss.item()
+        epochs_loss += current_loss
+
+        if batch == 0 or (batch+1)%100 == 0 or batch == len(training_dataset.dataset)//BATCH_SIZE:
+            # Recording the total loss
+            batch_total_train_loss_list.append(current_loss)
+            # Recording each losses 
+            batch_train_losses_list.append(losses)
+            # Recording class accuracy 
+            batch_train_class_acc.append(train_classes_acc)
+
+            utils.pretty_print(batch, len(training_dataset.dataset), current_loss, losses, train_classes_acc)
+
+            ############### Compute validation metrics each 100 batch ###########################################
+            _, bbox_true, bbox_preds, labels, label_preds = validation_loop(model, validation_dataset, S, device)
+            
+            ### Validation MSE score
+            mse_score = MSE(bbox_true, bbox_preds)
+
+            ### Validation accuracy
+            acc = class_acc(bbox_true, labels, label_preds)
+
+            ### Validation confidence_score
+            mse_confidence_score = MSE_confidenceScore(bbox_true, bbox_preds)
+
+            batch_val_MSE_box_list.append(mse_score)
+            batch_val_confscore_list.append(mse_confidence_score)
+            batch_val_class_acc.append(acc)
+
+            print(f"| MSE validation box loss : {mse_score:.5f}")
+            print(f"| MSE validation confidence score : {mse_confidence_score:.5f}")
+            print(f"| Validation class acc : {acc*100:.2f}%")
+            print("\n\n")
+            #####################################################################################################
+
+            if batch == len(training_dataset.dataset)//BATCH_SIZE:
+                print(f"Total elapsed time for training : {datetime.timedelta(seconds=timer()-begin_time)}")
+                print(f"Mean training loss for this epoch : {epochs_loss / len(training_dataset):.5f}")
+                print("\n\n")
+                logging.info(f"Epoch {epoch+1}/{EPOCHS}")
+                logging.info(f"***** Training loss : {epochs_loss / len(training_dataset):.5f}")
+                logging.info(f"***** MSE validation box loss : {mse_score:.5f}")
+                logging.info(f"***** MSE validation confidence score : {mse_confidence_score:.5f}")
+                logging.info(f"***** Validation class acc : {acc*100:.2f}%")
+
+### Saving results
+path_save_model = f"yolo_model_{epoch}epochs"
+utils.save_model(model, path_save_model, SAVE_MODEL)
+
+pickle_val_results = {
+"batch_val_MSE_box_list":batch_val_MSE_box_list,
+"batch_val_confscore_list":batch_val_confscore_list,
+"batch_val_class_acc":batch_val_class_acc
+}
+
+pickle_train_results = {
+    "batch_train_losses_list" : batch_train_losses_list,
+    "batch_train_class_acc" : batch_train_class_acc,
+}
+
+with open('train_results.pkl', 'wb') as pkl:
+    pickle.dump(pickle_train_results, pkl)
+
+with open('val_results.pkl', 'wb') as pkl:
+    pickle.dump(pickle_train_results, pkl)
+
+logging.info("End training.")
+#####################################################################################################
---
+++ YoloV1_Compagny_MealTrays/yolo_loss.py
@@ -0,0 +1,150 @@
+import torch
+
+class YoloLoss(torch.nn.Module):
+    def __init__(self, lambd_coord:int, lambd_noobj:float, device:torch.device, S:int=7, B:int=2):
+        super(YoloLoss, self).__init__()
+        self.LAMBD_COORD = lambd_coord
+        self.LAMBD_NOOBJ = lambd_noobj
+        self.S = S
+        self.B = B
+        self.device = device
+
+    def _coordloss(self, pred_coord_rcell, true_coord_rcell, isObject):
+        """
+        Args : 
+            pred_coord_rcell : torch.Tensor of shape (N, 2)
+            true_coord_rcell : torch.Tensor of shape (N, 2)
+            isObject : torch.Bool of shape (N,1)
+        Returns :
+            squared_error : torch.Tensor of shape (N)
+        """
+        xcr_hat, ycr_hat = pred_coord_rcell.permute(1,0)
+        xcr, ycr = true_coord_rcell.permute(1,0)
+
+        xcr_hat = torch.masked_fill(xcr_hat, isObject, 0)
+        ycr_hat = torch.masked_fill(ycr_hat, isObject, 0)
+        xcr = torch.masked_fill(xcr, isObject, 0)
+        ycr = torch.masked_fill(ycr, isObject, 0)
+
+        squared_error = torch.pow(xcr - xcr_hat,2) + torch.pow(ycr - ycr_hat,2)
+        return squared_error
+
+    def _sizeloss(self, pred_size, true_size, isObject):
+        """
+        Args :
+            pred_size : torch.Tensor of shape (N, 2)
+            true_size : torch.Tensor of shape (N, 2)
+            isObject : torch.Bool of shape (N,1)
+        Returns : 
+            root_squared_error : torch.Tensor of shape (N)
+        """
+        wr_hat, hr_hat = pred_size.permute(1,0)
+        wr, hr = true_size.permute(1,0)
+
+        wr_hat = torch.masked_fill(wr_hat, isObject, 0)
+        hr_hat = torch.masked_fill(hr_hat, isObject, 0)
+        wr = torch.masked_fill(wr, isObject, 0)
+        hr = torch.masked_fill(hr, isObject, 0)
+
+        wr_hat = wr_hat.clamp(min=0)
+        hr_hat = hr_hat.clamp(min=0)
+
+        root_squared_error_w = torch.pow(torch.sqrt(wr) - torch.sqrt(wr_hat),2)
+        root_squared_error_h = torch.pow(torch.sqrt(hr) - torch.sqrt(hr_hat),2)
+        root_squared_error = root_squared_error_w + root_squared_error_h
+        return root_squared_error
+
+    def _confidenceloss(self, pred_c, true_c, isObject):
+        """
+        Args :
+            pred_c : torch.Tensor of shape (N,1)
+            true_c : torch.Tensor of shape (N,1)
+            isObject : torch.Bool of shape (N,1)
+        Return :
+            squared_error : torch.Tensor of shape (N)
+        """
+        pred_c = torch.masked_fill(pred_c, isObject, 0)
+        true_c = torch.masked_fill(true_c, isObject, 0)
+
+        squared_error = torch.pow(true_c - pred_c, 2)
+        return squared_error
+
+    def _classloss(self, pred_class, true_class, isObject):
+        """
+        Args :
+            pred_class : torch.Tensor of shape (N, 10)
+            true_class : torch.Tensor of shape (N, 10)
+            isObject : torch.Bool of shape (N,1)
+        Returns :
+            squared_error : torch.Tensor of shape (N)
+        """
+
+        squared_error = torch.pow(true_class - pred_class, 2)
+        squared_error = torch.sum(squared_error, dim=1)
+        return torch.masked_fill(squared_error, isObject, 0) 
+
+    def forward(self, pred_box:torch.Tensor, true_box:torch.Tensor):
+        """
+        Grid fowrard pass.
+
+        Args:
+            pred_box : torch.Tensor of shape (N, S, S, B*5 + C)
+                Batch predicted outputs containing 2 box infos (xcr_rcell, ycr_rcell, wr, hr, c)
+                and a one-hot encoded class for each grid cell.
+            true_box : torch.Tensor of shape (N, S, S, 5 + C)
+                Batch groundtrouth outputs containing xcr_rcell, ycr_rcell, wr, hr,
+                confident number c and one-hot encoded class for each grid cell.
+
+        Return:
+            loss : float
+                The batch loss value of the grid
+        """
+        BATCH_SIZE = len(pred_box)
+
+        ### Initialization of the losses
+        losses_list = ['loss_xy', 'loss_wh', 'loss_conf_obj', 'loss_conf_noobj', 'loss_class']
+        losses = {key : torch.zeros(BATCH_SIZE).to(self.device) for key in losses_list}
+        
+        ### Compute the losses for all images in the batch
+        for i in range(self.S):
+            for j in range(self.S):
+                for b in range(self.B):
+                    box_k = 5 * b
+                    ### bbox coordinates
+                    xy_hat = pred_box[:,i,j,box_k :(2+box_k)]
+                    xy = true_box[:,i,j,:2]
+                    wh_hat = pred_box[:,i,j,2+box_k :(4+box_k)]
+                    wh = true_box[:,i,j,2:4]
+                    
+                    ### confidence numbers
+                    pred_c = pred_box[:,i,j,4+box_k]
+                    true_c = true_box[:,i,j,4]
+
+                    ### objects to detect
+                    isObject = true_c.eq(0)
+
+                    ### sum the losses over the grid
+                    losses['loss_xy'] += self._coordloss(xy_hat, xy, isObject)
+                    losses['loss_wh'] += self._sizeloss(wh_hat, wh, isObject)
+                    losses['loss_conf_obj'] += self._confidenceloss(pred_c, true_c, isObject)
+                    losses['loss_conf_noobj'] += self._confidenceloss(pred_c, true_c, torch.logical_not(isObject))
+
+                ### class labels
+                pred_class = pred_box[:,i,j,10:]
+                true_class = true_box[:,i,j,5:]
+                losses['loss_class'] += self._classloss(pred_class, true_class, isObject)
+     
+        ### Yolo_v1 loss over the batch, shape : (BATCH_SIZE)
+        for key, value in losses.items():
+            losses[key] = torch.sum(value)/BATCH_SIZE
+
+        loss = self.LAMBD_COORD * losses['loss_xy'] \
+                + self.LAMBD_COORD * losses['loss_wh'] \
+                + losses['loss_conf_obj'] \
+                + self.LAMBD_NOOBJ * losses['loss_conf_noobj'] \
+                + losses['loss_class']
+
+        loss = torch.sum(loss) / BATCH_SIZE
+
+        assert torch.isnan(loss)==False, "Error : loss turns to NaN."   
+        return losses, loss
