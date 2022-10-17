459bb7fdd93a6335bc213cbdb93d02728dd23ebf
ThOpaque_MBA <thierry.kss@posteo.net>
2022-10-17 10:03:54 +0200

    Some edits and make it compatible with 2 bboxes

--- YoloV1_Compagny_MealTrays/metrics.py
+++ YoloV1_Compagny_MealTrays/metrics.py
@@ -1,8 +1,8 @@
 import torch
 import utils
+import IoU
 
-
-def class_acc(target:torch.Tensor, prediction:torch.Tensor, B=2)->float:
+def class_acc(target:torch.Tensor, prediction:torch.Tensor, B:int=2)->float:
     """
     Compute class accuracy using cells WITH object.
 
@@ -11,30 +11,29 @@ def class_acc(target:torch.Tensor, prediction:torch.Tensor, B=2)->float:
             Groundtruth tensor
         prediction : torch.Tensor of shape (N,S,S,B*(4+1)+C) -> (N,7,7,18)
             Predicted tensor
+        B : int
+            Number of predicted bounding box
 
     Returns:
         acc : float
             Class accuracy between 0 and 1.
     """
-    ### Current batch size
-    BATCH_SIZE = len(target)
-
     ### Retrieve indices with object
     N, cells_i, cells_j = utils.get_cells_with_object(target)
 
     ### Applying softmax to get label probabilities only in cells with object
-    softmax_pred_classes = torch.softmax(prediction[N, cells_i, cells_j, B*(4+1):], dim=1)
+    softmax_pred_classes = torch.softmax(prediction[N, cells_i, cells_j, B*5:], dim=1)
     labels_pred = torch.argmax(softmax_pred_classes, dim=-1)
 
     ### Get true labels
-    labels_true = torch.argmax(target[N, cells_i, cells_j, (4+1):], dim=-1)
-
+    labels_true = torch.argmax(target[N, cells_i, cells_j, 5:], dim=-1)
+    
     ### Mean of the right predictions where there should be an object
-    acc = (1/BATCH_SIZE) * torch.sum(labels_true == labels_pred)
+    acc = (1/len(N)) * torch.sum(labels_true == labels_pred)
     return acc.item()
 
 
-def MSE(target:torch.Tensor, prediction:torch.Tensor, B=2)->float:
+def MSE(target:torch.Tensor, prediction:torch.Tensor, S=7, B=2)->float:
     """
     Mean Square Error along bbox coordinates and sizes in the cells containing an object
 
@@ -46,26 +45,53 @@ def MSE(target:torch.Tensor, prediction:torch.Tensor, B=2)->float:
     
     Returns:
         MSE_score : float
-            MSE value between 0 and 1
+            MSE value
     """
     ### Current batch size
     BATCH_SIZE = len(target)
 
     ### Retrieve indices with object
     N, cells_i, cells_j = utils.get_cells_with_object(target)
-
-    ### Get box info only in cells with object
-    target = target[N, cells_i, cells_j, 0:4]
-
-    MSE_score = 0
+    
+    ### Compute the losses for all images in the batch
+    iou_box = []
+    target_box_abs = IoU.relative2absolute(target[:,:,:,:5], cells_i, cells_j) # -> (N,4) ### trying smth by using relative2absolute_pred as converter for target
     for b in range(B):
-        box_k = b*(4+1)
-        prediction_box_k = prediction[N, cells_i, cells_j, 0+box_k :4+box_k]
-        MSE_score += torch.pow(target - prediction_box_k,2)
+        box_k = 5*b
+        prediction_box_abs = IoU.relative2absolute(prediction[:,:,:, box_k : 5+box_k], cells_i, cells_j) # -> (N,4)
+        iou = IoU.intersection_over_union(target_box_abs, prediction_box_abs) # -> (N,1)
+        iou_box.append(iou) # -> [iou_box1:(N), iou_box:(N)]                    
+    
+    ### TODO comment
+    iou_mask = torch.gt(iou_box[0], iou_box[1])
+    iou_mask = iou_mask.to(torch.int64)
+    idx = 5*iou_mask #if 0 -> box1 infos, if 5 -> box2 infos
+
+    ### bbox coordinates relating to the box with the largest IoU
+    ### note : python doesn't like smth like a[N,i,j, arr1:arr2]
+    x_hat = prediction[N, cells_i, cells_j, idx]                
+    y_hat = prediction[N, cells_i, cells_j, idx+1]                
+    w_hat = prediction[N, cells_i, cells_j, idx+2]
+    h_hat = prediction[N, cells_i, cells_j, idx+3]
+    
+    xywh_hat = torch.stack((x_hat, y_hat, w_hat, h_hat), dim=-1)
+    xywh = target[:,cells_i, cells_j, :4]
+
+    MSE_score = torch.pow(xywh - xywh_hat,2)
     
     ### Mean of the box info MSEs
     MSE_score = (1/BATCH_SIZE) * torch.sum(MSE_score)
-    return MSE_score.item()
+    return MSE_score.item() 
+    
+    # ### Get box info only in cells with object
+    # target = target[N, cells_i, cells_j, 0:4]
+    # MSE_score = 0
+    # for b in range(B):
+    #     box_k = b*4 #discard confident score
+    #     prediction_box_k = prediction[N, cells_i, cells_j, 0+box_k :5+box_k]
+    #     MSE_score += torch.pow(target - prediction_box_k,2)
+    
+
 
 
 def MSE_confidenceScore(target:torch.Tensor, prediction:torch.Tensor, S:int=7, B:int=2)->float:
@@ -88,14 +114,39 @@ def MSE_confidenceScore(target:torch.Tensor, prediction:torch.Tensor, S:int=7, B
     ### Current batch size
     BATCH_SIZE = len(target)
 
-    mse_confidence_score = 0
-    for i in range(S):
-        for j in range(S):
-            for b in range(B):
-                box_k = b*(4+1)
-                # iou = intersection_over_union(target[:,i,j], prediction[:,i,j]).to(device)
-                mse_confidence_score += torch.pow(target[:,i,j,4] - prediction[:,i,j,4+box_k], 2) #* iou,  2)
+    ### Retrieve indices with object
+    N, cells_i, cells_j = utils.get_cells_with_object(target)
+    
+    ### Compute the losses for all images in the batch
+    iou_box = []
+    target_box_abs = IoU.relative2absolute(target[:,:,:,:5], cells_i, cells_j) # -> (N,4) ### trying smth by using relative2absolute_pred as converter for target
+    for b in range(B):
+        box_k = 5*b
+        prediction_box_abs = IoU.relative2absolute(prediction[:,:,:, box_k : 5+box_k], cells_i, cells_j) # -> (N,4)
+        iou = IoU.intersection_over_union(target_box_abs, prediction_box_abs) # -> (N,1)
+        iou_box.append(iou) # -> [iou_box1:(N), iou_box:(N)]                    
+    
+    ### TODO comment
+    iou_mask = torch.gt(iou_box[0], iou_box[1])
+    iou_mask = iou_mask.to(torch.int64)
+    idx = 5*iou_mask #if 0 -> box1 infos, if 5 -> box2 infos
+
+    ### confident score related to the box with the largest IoU
+    prediction_confident_score = prediction[N, cells_i, cells_j, idx+4]                
+    target_confident_score = target[:,cells_i, cells_j, 4]
+
+    # iou = intersection_over_union(target[:,i,j], prediction[:,i,j]).to(device)
+    mse_confidence_score = torch.pow(target_confident_score - prediction_confident_score, 2) #* iou,  2)
 
     ### Mean of the confidence scores
     mse_confidence_score = (1/BATCH_SIZE) * torch.sum(mse_confidence_score)
     return mse_confidence_score.item()
+
+    # mse_confidence_score = 0
+    # for i in range(S):
+    #     for j in range(S):
+    #         for b in range(B):
+    #             box_k = b*5
+    #             # iou = intersection_over_union(target[:,i,j], prediction[:,i,j]).to(device)
+
+
