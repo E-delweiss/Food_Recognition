406d8c6f6f36b139f0b9aa2dd496caff414510d6
ThOpaque_MBA <thierry.kss@posteo.net>
2022-10-18 22:02:20 +0200

    Minor edits and add end training time.

--- YoloV1_Compagny_MealTrays/train.py
+++ YoloV1_Compagny_MealTrays/train.py
@@ -8,20 +8,23 @@ import torch
 import utils
 from yolo_loss import YoloLoss
 from mealtrays_dataset import get_training_dataset, get_validation_dataset
-from darknet import YoloV1
+# from darknet_like import YoloV1
+# from darknet import YoloV1
+from tinydarknet import TinyYolo as YoloV1
 from metrics import MSE, MSE_confidenceScore, class_acc
 from validation import validation_loop
 
 learning_rate = 0.001
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 SAVE_MODEL = True
 SAVE_LOSS = True
-utils.create_logging()
-device = utils.device()
+utils.create_logging(prefix="tinydarknet")
+device = utils.device(verbose=1)
 logging.info(f"Learning rate = {learning_rate}")
 logging.info(f"Batch size = {BATCH_SIZE}")
 
-model = YoloV1(448, S=7, C=8, B=2)
+model = YoloV1(in_channels=3, S=7, C=8, B=2)
+# model = YoloV1(448, S=7, C=8, B=2)
 model = model.to(device)
 optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate, weight_decay=0.0005)
 loss_yolo = YoloLoss(lambd_coord=5, lambd_noobj=0.5, S=7, device=device)
@@ -29,7 +32,7 @@ logging.info(f"Using optimizer : {optimizer}")
 
 training_dataloader = get_training_dataset(BATCH_SIZE)
 validation_dataloader = get_validation_dataset()
-DO_VALIDATION = False
+DO_VALIDATION = True
 
 # img, target = next(iter(training_dataloader))
 
@@ -38,14 +41,16 @@ DO_VALIDATION = False
 delta_time = datetime.timedelta(hours=1)
 timezone = datetime.timezone(offset=delta_time)
 
-t = datetime.datetime.now(tz=timezone)
-str_t = '{:%Y-%m-%d %H:%M:%S}'.format(t)
-print(f"[START] : {str_t} :")
+time_formatted = datetime.datetime.now(tz=timezone)
+time_formatted = '{:%Y-%m-%d %H:%M:%S}'.format(time_formatted)
+start_time = datetime.datetime.now()
+
 print(f"[Training on] : {str(device).upper()}")
 print(f"Learning rate : {optimizer.defaults['lr']}")
 logging.info("Start training")
+logging.info(f"[START] : {time_formatted}")
 
-EPOCHS = 10
+EPOCHS = 50
 S = 7
 
 ################################################################################
@@ -64,8 +69,7 @@ for epoch in range(EPOCHS):
     epochs_loss = 0.
     
     print("-"*20)
-    str_t = '{:%Y-%m-%d %H:%M:%S}'.format(t)
-    print(" "*5 + f"{str_t} : EPOCH {epoch+1}/{EPOCHS}")
+    print(" "*5 + f"EPOCH {epoch+1}/{EPOCHS}")
     print(" "*5 + f"Learning rate : lr = {optimizer.defaults['lr']}")
     print("-"*20)
 
@@ -100,7 +104,7 @@ for epoch in range(EPOCHS):
         current_loss = loss.item()
         epochs_loss += current_loss
 
-        if batch == 0 or (batch+1)%1 == 0 or batch == len(training_dataloader.dataset)//BATCH_SIZE:
+        if batch == 0 or (batch+1)%8 == 0 or batch == len(training_dataloader.dataset)//BATCH_SIZE:
             # Recording the total loss
             batch_total_train_loss_list.append(current_loss)
             # Recording each losses 
@@ -137,17 +141,16 @@ for epoch in range(EPOCHS):
             #####################################################################################################
 
             if batch == len(training_dataloader.dataset)//BATCH_SIZE:
-                print(f"Total elapsed time for training : {datetime.timedelta(seconds=timer()-begin_time)}")
                 print(f"Mean training loss for this epoch : {epochs_loss / len(training_dataloader):.5f}")
                 print("\n\n")
                 logging.info(f"Epoch {epoch+1}/{EPOCHS}")
                 logging.info(f"***** Training loss : {epochs_loss / len(training_dataloader):.5f}")
                 logging.info(f"***** MSE validation box loss : {mse_score:.5f}")
                 logging.info(f"***** MSE validation confidence score : {mse_confidence_score:.5f}")
-                logging.info(f"***** Validation class acc : {acc*100:.2f}%")
-
+                logging.info(f"***** Validation class acc : {acc*100:.2f}%\n")
+                
 ### Saving results
-path_save_model = f"yolo_model_{epoch}epochs"
+path_save_model = f"yolo_model_{epoch+1}epochs"
 utils.save_model(model, path_save_model, SAVE_MODEL)
 
 pickle_val_results = {
@@ -168,5 +171,7 @@ if SAVE_LOSS:
     with open('val_results.pkl', 'wb') as pkl:
         pickle.dump(pickle_val_results, pkl)
 
+end_time = datetime.datetime.now()
+logging.info('Time duration: {}.'.format(end_time - start_time))
 logging.info("End training.")
 #####################################################################################################
