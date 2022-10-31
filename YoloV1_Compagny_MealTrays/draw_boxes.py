06e25f5e0dbca19f38d9fdbfc8eea88fdc5b175c
ThOpaque_MBA <thierry.kss@posteo.net>
2022-10-31 22:20:26 +0100

    Work on draw boxes.

--- YoloV1_Compagny_MealTrays/draw_boxes.py
+++ YoloV1_Compagny_MealTrays/draw_boxes.py
@@ -1,19 +1,18 @@
-import os
+from cProfile import label
 from configparser import ConfigParser
 
 import torch
 import torchvision
 import numpy as np
 import matplotlib.pyplot as plt
-from YoloV1_Compagny_MealTrays.train import IN_CHANNEL 
+from torchvision.utils import draw_bounding_boxes
 
-from resnet101 import YoloV1
+from darknet import YoloV1
 from validation import validation_loop
 from mealtrays_dataset import get_validation_dataset
 import IoU
 import utils
 # import NMS
-import draw_boxes_utils
 
 config = ConfigParser()
 config.read("config.ini")
@@ -21,140 +20,90 @@ IN_CHANNEL = config.getint("MODEL", "in_channel")
 S = config.getint("MODEL", "GRID_SIZE")
 C = config.getint("MODEL", "NB_CLASS")
 B = config.getint("MODEL", "NB_BOX")
+model_weights = config.get("PLOTS", "PT_FILE")
+
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = torchvision.transforms.functional.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    plt.show()
 
 def draw_boxes(
-    img:torch.Tensor, target:torch.Tensor, prediction:torch.Tensor, iou_threshold:float=0.6, nb_sample:int=10, title:str=""
+    img:torch.Tensor, target:torch.Tensor, prediction:torch.Tensor, iou_threshold:float=0.6, nb_sample:int=3, title:str="", PRINT_ALL=True
     ):
     """
-    Display 'n' images and draw groundtruth and predicted bounding boxes on it.
-    Show predicted class and IoU value between the two boxes.
-
-    Args:
-        img (torch.Tensor of shape (N,1,75,75))
-            Images from validation dataset.
-        TODO
-        iou_threshold (float, optional)
-            Defaults to 0.6.
-        nb_sample (int, optional)
-            Nb of sample to display. Defaults to 10.
-        title (str, optional)
-            Title of the plot. Defaults to "".
-
-    Returns:
-        None
+    TODO
     """
+    color_dict = {'Assiette':'b', 'Entree':'g', 'Pain':'r', 'Boisson':'c', 
+            'Yaourt':'darkred', 'Dessert':'k', 'Fruit':'m', 'Fromage':'y'}
+    label_dict = {0:'Assiette', 1:'Entree', 2:'Pain', 3:'Boisson', 
+        4:'Yaourt', 5:'Dessert', 6:'Fruit', 7:'Fromage'}
 
     BATCH_SIZE = len(img)
-    indexes = np.random.choice(BATCH_SIZE, size=nb_sample)
-    N = range(nb_sample)
+    idx = np.random.randint(0, BATCH_SIZE)
 
     ### Choose image & rescale pixel values to un-normed values (from 0 -black- to 255 -white-)
-    n_imgs = img[indexes]
-    # mean=(0.4168, 0.4055, 0.3838), std=(0.3475, 0.3442, 0.3386)
+    img_idx = img[idx]
     inv_normalize = torchvision.transforms.Normalize(
         mean=[-0.4168/0.3475, -0.4055/0.3442, -0.3838/0.3386],
         std=[1/0.3475, 1/0.3442, 1/0.3386]
         )
-    n_imgs = inv_normalize(n_imgs).numpy()
-    n_imgs = n_imgs * 255.0
-    # n_imgs = n_imgs.reshape(nb_sample, 3, 448, 448)
+    img_idx = inv_normalize(img_idx) * 255.0
+    img_idx = img_idx.to(torch.uint8)
 
     ### Choose label & argmax of one-hot vectors.
-    n_label_true = target[indexes,:,:,5:]
-    n_label_true_argmax = torch.argmax(n_label_true, dim=-1).numpy()
+    label_true = target[idx,:,:,5:]
+    label_true_argmax = torch.argmax(label_true, dim=-1)
 
-    n_label_pred = prediction[indexes,:,:,10:]
-    n_label_pred_argmax = torch.argmax(torch.softmax(n_label_pred, dim=-1), dim=-1).numpy() #(N,S,S,8) -> (N,S,S,1)
+    label_pred = prediction[idx,:,:,10:]
+    label_pred_argmax = torch.argmax(torch.softmax(label_pred, dim=-1), dim=-1) #(N,S,S,8) -> (N,S,S,1)
 
     ### Groundtruth & preds boxes
-    n_box_true = target[indexes, :, :, :5]
-    n_box_pred = prediction[indexes, :, :, :10] ### !!!
+    box_true = target[idx, :, :, :5].unsqueeze(0)
+    box_pred = prediction[idx, :, :, :10] ### !!!
 
 #### ------------------------ ##########
 
-    ### Turn to absolute coords and get indices of box positions after NMS
-    N, cell_i, cell_j = utils.get_cells_with_object(target)
-    n_box_true_abs = IoU.relative2absolute(n_box_true, N, cell_i, cell_j)
-
-    iou_box = []
-    for b in range(B):
-        box_k = 5*b
-        n_box_pred = IoU.relative2absolute(n_box_pred[:,:,:, box_k : 5+box_k], N, cell_i, cell_j) # -> (N,4)
-        iou = IoU.intersection_over_union(n_box_true_abs, n_box_pred) # -> (N,1)
-        iou_box.append(iou) # -> [iou_box1:(N), iou_box:(N)]    
-
-    iou_mask = torch.gt(iou_box[0], iou_box[1])
-    iou_mask = iou_mask.to(torch.int64)
-    idx = 5*iou_mask #if 0 -> box1 infos, if 5 -> box2 infos
-    x_pred_abs = n_box_pred[N, cell_i, cell_j, idx]                
-    y_pred_abs = n_box_pred[N, cell_i, cell_j, idx+1]                
-    w_pred_abs = n_box_pred[N, cell_i, cell_j, idx+2]
-    h_pred_abs = n_box_pred[N, cell_i, cell_j, idx+3] ## ???
-
-    n_box_pred_abs = torch.stack((x_pred_abs, y_pred_abs, w_pred_abs, h_pred_abs), dim=-1)
-
-
-    n_box_pred_abs, temp_indices = NMS.non_max_suppression(n_box_pred, n_label_pred, 0.6)
-    n_box_pred_abs = n_box_pred_abs[N, temp_indices[:,0], temp_indices[:,1]]
+    ### Get cells with object
+    N, cell_i, cell_j = utils.get_cells_with_object(box_true)
     
-    ### Get label predictions with respect to prediction boxes
-    n_label_pred_argmax = n_label_pred_argmax[N, temp_indices[:,0], temp_indices[:,1]]
+    ### Convert to absolute coord
+    box_true_abs = IoU.relative2absolute(box_true, N, cell_i, cell_j)
 
-    ### Get IoU between pred and true boxes
-    n_iou = IoU.intersection_over_union(n_box_true_abs, n_box_pred_abs)
+    ### Retrive label names as a list 
+    label_true_argmax = label_true_argmax[cell_i, cell_j]
+    label_list = [label_dict.get(label.item()) for label in label_true_argmax]
 
-    ### draw_boxes_utils uses arrays only
-    n_box_true_abs = n_box_true_abs.numpy()
-    n_box_pred_abs = n_box_pred_abs.numpy()
+    ### Choose the best predicted bounding box
+    #NMS !!!
 
-    ### Set plot configs
-    all_plots = plt.figure(figsize=(20, 4))
-    plt.title(title, fontweight='bold')
-    plt.yticks([])
-    plt.xticks([])
+    # n_box_pred_abs = IoU.relative2absolute(box_pred[:,:,:,:5], N, cell_i, cell_j) #TBM
+    N_unique = torch.unique(N)
+    N_freq = torch.bincount(N)
+    N_cumsum = torch.cumsum(N_freq, dim=0)
+    N_sort = torch.stack((N_unique, N_freq, N_cumsum), dim=-1)
 
-    print("Draw boxes...")
-    for k in N :
-        subplots = all_plots.add_subplot(1, nb_sample, k+1)
-
-        # Dispay label name (0,1,2,...,9)
-        plt.xlabel(f"Digit : {n_label_pred_argmax[k]}", fontweight='bold')
-        
-        # Display in red if pred is false
-        if n_label_pred_argmax[k] != n_label_true_argmax[k]:
-            subplots.xaxis.label.set_color('red')
-        else : 
-            subplots.xaxis.label.set_color('green')
-        
-        # Draw true and pred bounding boxes
-        img_to_draw = draw_boxes_utils.draw_bounding_boxes_on_image_array(
-            image = n_imgs[k], 
-            box_true = n_box_true_abs[k], 
-            box_pred = n_box_pred_abs[k],
-            color = ["white", "red"], 
-            display_str_list = ["true", "pred"]
-            )
-
-        # Display iou in red if iou < threshold
-        if (n_iou[k] < iou_threshold):
-            color = "red"
-        else :
-            color = "green"
-        subplots.text(0.10, -0.4, f"iou: {n_iou[k].item():.3f}", color=color, transform=subplots.transAxes, fontweight='bold')
-            
-    plt.show()
- 
+    start = N_sort[0,2]-N_sort[0,1]
+    stop = N_sort[0,2]
+    drawn_boxes = draw_bounding_boxes(img_idx.to(torch.uint8), box_true_abs[start: stop], labels=label_list, width=2, font='Arial Bold', font_size=15)
+    show(drawn_boxes)
    
 
 if __name__ == "__main__":
-    os.chdir("WarmingUp_with_MNIST")
+    # os.chdir("WarmingUp_with_MNIST")
 
     print("Load model...")
-    resnet101 = YoloV1(IN_CHANNEL, S, C, B)
-    resnet101.load_state_dict(torch.load("yoloPlato_resnet101_150epochs_25102022_06h17.pt"))
+    darknet = YoloV1(IN_CHANNEL, S=S, B=B, C=C)
+    darknet.load_state_dict(torch.load("results/yoloPlato_darknet_150epochs_30102022_18h11.pt"))
     
     print("Validation loop")
-    validation_dataset = get_validation_dataset(64)
-    img, target, prediction = validation_loop(resnet101, validation_dataset)
+    validation_dataset = get_validation_dataset(8, isNormalize=True, isAugment=False)
+    img, target, prediction = validation_loop(darknet, validation_dataset, ONE_BATCH=True)
 
-    draw_boxes(img, label_true, label_pred, box_true, box_pred, title="MNIST groundtruth vs predictions")
+    draw_boxes(img, target, prediction)
--- YoloV1_Compagny_MealTrays/draw_boxes_utils.py
+++ YoloV1_Compagny_MealTrays/draw_boxes_utils.py
@@ -1,13 +1,17 @@
-import numpy as np
 import os
+
+import numpy as np
 import PIL
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import torchvision
 
 def draw_bounding_boxes_on_image_array(
     image:np.ndarray, box_true:np.ndarray, box_pred:np.ndarray, color:list=[],thickness:int=1
     ):
     """
     Draws bounding boxes on PIL image.
+    TODO
 
     Args:
         image (np.ndarray of shape (N,1,75,75))
@@ -35,72 +39,33 @@ def draw_bounding_boxes_on_image_array(
     MATPLOTLIB_FONT_DIR = os.path.join(os.path.dirname(plt.__file__), "mpl-data/fonts/ttf")
     ################################################################################
 
-    plt.yticks([])
-    plt.xticks([])
+    image_pil = torchvision.transforms.ToPILImage()(image)
 
-    image_pil = PIL.Image.fromarray(image)
-    rgbimg = PIL.Image.new("RGBA", image_pil.size)
-    rgbimg.paste(image_pil)
-    draw_bounding_boxes_on_image(rgbimg, box_true, box_pred, color, thickness)
-    plt.imshow(np.array(rgbimg))
+    ### Set plot configs
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(image_pil)
+    ax.set_xticks = []
+    ax.set_yticks = []
+
+    for k_box in range(len(box_true)):
+        xmin = box_true[k_box, 0]
+        ymin = box_true[k_box, 1]
+        w = box_true[k_box, 2] - xmin
+        h = box_true[k_box, 3] - ymin 
+
+        # for k, _ in enumerate(box_pred):
+        #     draw_ONE_bounding_box_on_image(image, box_pred[k,0], box_pred[k,1], box_pred[k,2], box_pred[k,3], color=color_list[1], thickness=thickness)
 
 
-
-def draw_bounding_boxes_on_image(
-    image:PIL.Image, box_true:np.ndarray, box_pred:np.ndarray, color_list:list=[], thickness:int=1
-    ):
-    """
-    Draws bounding boxes on image. Calls the module draw_ONE_bounding_box_on_image twice to plot 
-    both groundtruth and predicted bounding boxes.
-
-    Args:
-        image (PIL.Image)
-            PIL Image on which the 75x75 image has been pasted.
-        box_true (np.ndarray of shape (N,4))
-            Groundtruth boxes with absolute coordinates.
-        box_pred (np.ndarray)
-            Predicted boxes with absolute coordinates.
-        color (list, optional)
-            Color of bounding boxes. Defaults to [].
-        thickness (int, optional)
-            Thickness of lines. Defaults to 1.
-
-    Returns:
-    None.
-    """
-    draw_ONE_bounding_box_on_image(image, box_true[0], box_true[1], box_true[2], box_true[3], color=color_list[0], thickness=thickness)
-    draw_ONE_bounding_box_on_image(image, box_pred[0], box_pred[1], box_pred[2], box_pred[3], color=color_list[1], thickness=thickness)
-
-                                                            
-
-def draw_ONE_bounding_box_on_image(
-    image, xmin:float, ymin:float, xmax:float, ymax:float, color:list=[], thickness:int=1
-    ):
-    """
-    Draw lines on PIL Image.
-    
-    Args:
-        image (PIL.Image)
-            PIL Image on which the 75x75 image has been pasted.
-        xmin (float)
-            Top left x value of bounding box.
-        ymin (float)
-            Top left y value of bounding box.
-        xmax (float)
-            Bottom right x value of bounding box.
-        tmax (float)
-            Bottom right y value of bounding box.
-        color (list, optional)
-            Color of bounding boxes. Defaults to [].
-        thickness (int, optional)
-            Thickness of lines. Defaults to 1.
-
-    Returns:
-        None
-    """
-    draw = PIL.ImageDraw.Draw(image)
-    left, right, top, bottom = xmin, xmax, ymin, ymax
-    draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=thickness, fill=color)
+        color = color[0] #color_dict.get(label_dict.get(label))
+        rect = patches.Rectangle((xmin, ymin), w, h, facecolor='none', edgecolor=color)
+        ax.add_patch(rect)
+        offset_x = 2
+        offset_y = 10
+        rect_txt = patches.Rectangle((xmin, ymin), w, 15, facecolor=color, edgecolor=color)
+        ax.add_patch(rect_txt)
+        # ax.text(xmin+offset_x, ymin+offset_y, label_dict.get(label), fontsize=8, color='w', family='monospace', weight='bold')
 
 
 
